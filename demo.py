d047f23b0cfad3beece76ed606e0ace74152276a
Adrian Bonnet <relex12@free.fr>
2023-06-25 10:27:51 +0200

    dempo.py: add different attempts, all fail

--- demo.py
+++ demo.py
@@ -9,6 +9,7 @@ a = randint(10, 1000)
 b = randint(10, 1000)
 c = randint(10, 1000)
 
+print ('Private keys')
 print(f'a = {a}')
 print(f'b = {b}')
 print(f'c = {c}')
@@ -18,6 +19,7 @@ A = (g**a) % p
 B = (g**b) % p
 C = (g**c) % p
 
+print ('Public keys')
 print(f'A = {A}')
 print(f'B = {B}')
 print(f'C = {C}')
@@ -30,6 +32,7 @@ kca = (A**c) % p
 kbc = (C**b) % p
 kcb = (B**c) % p
 
+print ('Pairwise session keys')
 print(f'kab = {kab}')
 print(f'kba = {kba}')
 print(f'kac = {kac}')
@@ -38,10 +41,42 @@ print(f'kbc = {kbc}')
 print(f'kcb = {kcb}')
 
 # kabc, kbac and kcab computed by Alice, Bob and Charles are multipartite secret keys
-kabc = (B**a * C**a) % p
-kbac = (A**b * C**b) % p
-kcac = (A**c * B**c) % p
+kabc1 = (B**a * C**a) % p
+kbac1 = (A**b * C**b) % p
+kcac1 = (A**c * B**c) % p
 
-print(f'kabc = {kabc}')
-print(f'kbac = {kbac}')
-print(f'kcac = {kcac}')
+print ('Three partite session keys, first attempt')
+print(f'kabc1 = {kabc1}')
+print(f'kbac1 = {kbac1}')
+print(f'kcac1 = {kcac1}')
+
+
+kabc2 = ((B+C)**a) % p
+kbac2 = ((A+C)**b) % p
+kcac2 = ((A+B)**c) % p
+
+print ('Three partite session keys, second attempt')
+print(f'kabc2 = {kabc2}')
+print(f'kbac2 = {kbac2}')
+print(f'kcac2 = {kcac2}')
+
+
+kabc3 = ((B*C)**a) % p
+kbac3 = ((A*C)**b) % p
+kcac3 = ((A*B)**c) % p
+
+print ('Three partite session keys, third attempt')
+print(f'kabc3 = {kabc3}')
+print(f'kbac3 = {kbac3}')
+print(f'kcac3 = {kcac3}')
+
+
+kabc4 = (B**a + C**a) % p
+kbac4 = (A**b + C**b) % p
+kcac4 = (A**c + B**c) % p
+
+print ('Three partite session keys, fourth attempt')
+print(f'kabc4 = {kabc4}')
+print(f'kbac4 = {kbac4}')
+print(f'kcac4 = {kcac4}')
+
