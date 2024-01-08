55324e1731441b970fe6458fec031d4c5ca1339a
Adrian Bonnet <relex12@free.fr>
2024-01-08 09:55:50 +0100

    main: uniform finite and continuous, use if else statement

--- main.py
+++ main.py
@@ -4,39 +4,46 @@ from random import randint
 
 from classes import *
 
+finite=True
 
-# C = EllipticCurve(a=-2, b=4)
-# C.plot(50)
+if not finite:
+    C = EllipticCurve(a=-2, b=4)
+    C.plot(50)
 
-# P = Point(C, frac(3), frac(5))
-# Q = Point(C, frac(-2), frac(0))
-# R = Point(C, frac(240), frac(3718))
-# R = 36*R
+    P = Point(C, frac(3), frac(5))
+    Q = Point(C, frac(-2), frac(0))
+    R = Point(C, frac(240), frac(3718))
+    R = 36*R
 
-# plot (P)
-# plot (Q)
-# plot (R)
+    plot (P)
+    plot (Q)
+    plot (R)
 
-# G = Point(C, frac(3), frac(5))
+    G = P
 
-# a = randint(10, 1000)
-# b = randint(10, 1000)
-# c = randint(10, 1000)
 
-# A = (a*G)
-# B = (b*G)
-# C = (c*G)
+    a = randint(10, 1000)
+    b = randint(10, 1000)
+    c = randint(10, 1000)
 
-# print (A)
-# plot (A, 'A')
-# print (B)
-# plot (B, 'B')
-# print (C)
-# plot (C, 'C')
+    A = (a*G)
+    B = (b*G)
+    C = (c*G)
 
-G = FiniteEllipticCurve(-5, 8, 37)
-G.plot()
+    print (A)
+    print (B)
+    print (C)
+    plot (A, 'A')
+    plot (B, 'B')
+    plot (C, 'C')
 
-P = FinitePoint(G, 5, 16)
+else:
+    C = FiniteEllipticCurve(-5, 8, 37)
+    C.plot()
+
+    P = FinitePoint(C, 5, 16)
+    Q = FinitePoint(C, 10, 25)
+    P.plot('P')
+    Q.plot('Q')
 
 plt.savefig("graph.png")
