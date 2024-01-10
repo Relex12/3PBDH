95a8e9d53350908dea363302d9e716ef71981fa5
Adrian Bonnet <relex12@free.fr>
2024-01-10 10:17:32 +0100

    demo: change a b c and A B C keys to ka kb kc and kA kB kC for further ECC importation

--- demo.py
+++ demo.py
@@ -4,35 +4,32 @@ from random import randint
 p = 997
 g = 7
 
-# a, b and c randomly chosen by Alice, Bob and Charles
-a = randint(10, 1000)
-b = randint(10, 1000)
-c = randint(10, 1000)
-
+# ka, kb and kc randomly chosen by Alice, Bob and Charles
 print ('Private keys')
-print(f'a = {a}')
-print(f'b = {b}')
-print(f'c = {c}')
+ka = randint(10, 1000)
+kb = randint(10, 1000)
+kc = randint(10, 1000)
+print(f'ka = {ka}')
+print(f'kb = {kb}')
+print(f'kc = {kc}')
 
-# A, B and C computed by Alice, Bob and Charles
-A = (g**a) % p
-B = (g**b) % p
-C = (g**c) % p
-
+# kA, kB and kC computed by Alice, Bob and Charles
 print ('Public keys')
-print(f'A = {A}')
-print(f'B = {B}')
-print(f'C = {C}')
+kA = (g**ka) % p
+kB = (g**kb) % p
+kC = (g**kc) % p
+print(f'kA = {kA}')
+print(f'kB = {kB}')
+print(f'kC = {kC}')
 
 # kab, kba, kac, kca, kbc and kcb computed by Alice, Bob and Charles are pairwise secret keys
-kab = (B**a) % p
-kba = (A**b) % p
-kac = (C**a) % p
-kca = (A**c) % p
-kbc = (C**b) % p
-kcb = (B**c) % p
-
 print ('Pairwise session keys')
+kab = (kB**ka) % p
+kba = (kA**kb) % p
+kac = (kC**ka) % p
+kca = (kA**kc) % p
+kbc = (kC**kb) % p
+kcb = (kB**kc) % p
 print(f'kab = {kab}')
 print(f'kba = {kba}')
 print(f'kac = {kac}')
@@ -41,41 +38,34 @@ print(f'kbc = {kbc}')
 print(f'kcb = {kcb}')
 
 # kabc, kbca and kcab computed by Alice, Bob and Charles are multipartite secret keys
-kabc1 = (B**a * C**a) % p
-kbca1 = (A**b * C**b) % p
-kcab1 = (A**c * B**c) % p
-
 print ('Three partite session keys, first attempt')
+kabc1 = (kB**ka * kC**ka) % p
+kbca1 = (kA**kb * kC**kb) % p
+kcab1 = (kA**kc * kB**kc) % p
 print(f'kabc1 = {kabc1}')
 print(f'kbca1 = {kbca1}')
 print(f'kcab1 = {kcab1}')
 
-
-kabc2 = ((B+C)**a) % p
-kbca2 = ((A+C)**b) % p
-kcab2 = ((A+B)**c) % p
-
 print ('Three partite session keys, second attempt')
+kabc2 = ((kB+kC)**ka) % p
+kbca2 = ((kA+kC)**kb) % p
+kcab2 = ((kA+kB)**kc) % p
 print(f'kabc2 = {kabc2}')
 print(f'kbca2 = {kbca2}')
 print(f'kcab2 = {kcab2}')
 
-
-kabc3 = ((B*C)**a) % p
-kbca3 = ((A*C)**b) % p
-kcab3 = ((A*B)**c) % p
-
 print ('Three partite session keys, third attempt')
+kabc3 = ((kB*kC)**ka) % p
+kbca3 = ((kA*kC)**kb) % p
+kcab3 = ((kA*kB)**kc) % p
 print(f'kabc3 = {kabc3}')
 print(f'kbca3 = {kbca3}')
 print(f'kcab3 = {kcab3}')
 
-
-kabc4 = (B**a + C**a) % p
-kbca4 = (A**b + C**b) % p
-kcab4 = (A**c + B**c) % p
-
 print ('Three partite session keys, fourth attempt')
+kabc4 = (kB**ka + kC**ka) % p
+kbca4 = (kA**kb + kC**kb) % p
+kcab4 = (kA**kc + kB**kc) % p
 print(f'kabc4 = {kabc4}')
 print(f'kbca4 = {kbca4}')
 print(f'kcab4 = {kcab4}')
