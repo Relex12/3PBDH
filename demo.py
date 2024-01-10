37082a16b10074b3b184dc931892eb911cc0fdb5
Adrian Bonnet <relex12@free.fr>
2024-01-10 15:31:15 +0100

    demo: add solve_ecc from solve.py, auto detect Point on the given curve, add help for every argument, minor fixes

--- demo.py
+++ demo.py
@@ -4,16 +4,35 @@ from argparse import ArgumentParser
 
 from classes import *
 
+
+def solve_ecc(a, b, p=None):
+    ret_list = []
+    x = 1
+    if p != None:
+        for i in range (p):
+            y = sqrt((x**3 + a*x + b) % p)
+            if y.is_integer():
+                ret_list.append((x,int(y)))
+            x+=1
+    else:
+        for i in range (10**5):
+            y = sqrt(x**3 + a*x + b)
+            if y.is_integer():
+                ret_list.append((x,int(y)))
+            x+=1
+    return ret_list
+
+
 parser = ArgumentParser()
 parser.add_argument("-v", "--version", action="version", version='1.0')
-parser.add_argument("-p", "--prime", metavar='PRIME', type=int, default=37, help="")    # 997, 509, 37
-parser.add_argument("-g", "--generator", metavar='NUM', type=int, default=7, help="")
-parser.add_argument("-a", metavar='NUM', type=int, default=-2, help="")
-parser.add_argument("-b", metavar='NUM', type=int, default=4, help="")
-parser.add_argument("-m", "--max-key", metavar='NUM', type=int, default=1000, help="")
-parser.add_argument("-c", "--continuous", action='store_true', help="")
-parser.add_argument("-d", "--debug", action='store_true', help="")
-parser.add_argument("-P", "--plot", action='store_true', help="")
+parser.add_argument("-p", "--prime", metavar='PRIME', type=int, default=509, help="prime number use for modulo in RSA and Finite fieds ECC")
+parser.add_argument("-g", "--generator", metavar='NUM', type=int, default=7, help="number used for Diffie-Hellman exchange")
+parser.add_argument("-a", metavar='NUM', type=int, default=-2, help="coefficient a of the Weierstrass form")
+parser.add_argument("-b", metavar='NUM', type=int, default=4, help="coefficient b of the Weierstrass form")
+parser.add_argument("-m", "--max-key", metavar='NUM', type=int, default=1000, help="maximum value for random private key generation")
+parser.add_argument("-c", "--continuous", action='store_true', help="run the regular Elliptic Curve take quite a long")
+parser.add_argument("-d", "--debug", action='store_true', help="print additionnal informations")
+parser.add_argument("-P", "--plot", action='store_true', help="plot graph with curve and points")
 args = parser.parse_args()
 
 
@@ -25,6 +44,7 @@ kc = randint(1, args.max_key)
 print(f'ka = {ka}')
 print(f'kb = {kb}')
 print(f'kc = {kc}')
+
 
 print ('RSA cryptography')
 # p and g are aggreed before the exchange
@@ -44,64 +64,50 @@ print(f'kC = {kC}')
 
 # kab, kba, kac, kca, kbc and kcb computed by Alice, Bob and Charles are pairwise secret keys
 print ('Pairwise session keys')
-kab = (kB**ka) % p
-kba = (kA**kb) % p
-kac = (kC**ka) % p
-kca = (kA**kc) % p
-kbc = (kC**kb) % p
-kcb = (kB**kc) % p
-print(f'kab = {kab}')
-print(f'kba = {kba}')
-print(f'kac = {kac}')
-print(f'kca = {kca}')
-print(f'kbc = {kbc}')
-print(f'kcb = {kcb}')
+print(f'kab = {(kB**ka) % p}')
+print(f'kba = {(kA**kb) % p}')
+print(f'kac = {(kC**ka) % p}')
+print(f'kca = {(kA**kc) % p}')
+print(f'kbc = {(kC**kb) % p}')
+print(f'kcb = {(kB**kc) % p}')
 
 # kabc, kbca and kcab computed by Alice, Bob and Charles are multipartite secret keys
 print ('Three partite session keys, first attempt')
-kabc1 = (kB**ka * kC**ka) % p
-kbca1 = (kA**kb * kC**kb) % p
-kcab1 = (kA**kc * kB**kc) % p
-print(f'kabc1 = {kabc1}')
-print(f'kbca1 = {kbca1}')
-print(f'kcab1 = {kcab1}')
+print(f'kabc1 = {(kB**ka * kC**ka) % p}')
+print(f'kbca1 = {(kA**kb * kC**kb) % p}')
+print(f'kcab1 = {(kA**kc * kB**kc) % p}')
 
 print ('Three partite session keys, second attempt')
-kabc2 = ((kB+kC)**ka) % p
-kbca2 = ((kA+kC)**kb) % p
-kcab2 = ((kA+kB)**kc) % p
-print(f'kabc2 = {kabc2}')
-print(f'kbca2 = {kbca2}')
-print(f'kcab2 = {kcab2}')
+print(f'kabc2 = {((kB+kC)**ka) % p}')
+print(f'kbca2 = {((kA+kC)**kb) % p}')
+print(f'kcab2 = {((kA+kB)**kc) % p}')
 
 print ('Three partite session keys, third attempt')
-kabc3 = ((kB*kC)**ka) % p
-kbca3 = ((kA*kC)**kb) % p
-kcab3 = ((kA*kB)**kc) % p
-print(f'kabc3 = {kabc3}')
-print(f'kbca3 = {kbca3}')
-print(f'kcab3 = {kcab3}')
+print(f'kabc3 = {((kB*kC)**ka) % p}')
+print(f'kbca3 = {((kA*kC)**kb) % p}')
+print(f'kcab3 = {((kA*kB)**kc) % p}')
 
 print ('Three partite session keys, fourth attempt')
-kabc4 = (kB**ka + kC**ka) % p
-kbca4 = (kA**kb + kC**kb) % p
-kcab4 = (kA**kc + kB**kc) % p
-print(f'kabc4 = {kabc4}')
-print(f'kbca4 = {kbca4}')
-print(f'kcab4 = {kcab4}')
+print(f'kabc4 = {(kB**ka + kC**ka) % p}')
+print(f'kbca4 = {(kA**kb + kC**kb) % p}')
+print(f'kcab4 = {(kA**kc + kB**kc) % p}')
 
 
 if args.continuous:
     print ('Elliptic curve cryptography')
-    a = -2 # args.a
-    b = 4  # args.b
+    a = args.a
+    b = args.b
     curve = EllipticCurve(a, b)
     if args.debug:
         print(curve)
     if args.plot:
         curve.plot(20)
 
-    G = Point(curve, frac(3), frac(5), 'G')
+    solutions = solve_ecc(a, b)
+    if solutions == []:
+        raise Error(f'Curve {curve} does not seem to have solutions.')
+    x, y = solutions[0]
+    G = Point(curve, frac(x), frac(y), 'G')
     if args.debug:
         print(f'G = {G}')
     if args.plot:
@@ -120,12 +126,12 @@ if args.continuous:
         kC.plot('kC')
 
     print ('Pairwise session keys')
-    kab = ka*kb*G
-    kba = kb*ka*G
-    kac = ka*kc*G
-    kca = kc*ka*G
-    kbc = kb*kc*G
-    kcb = kc*kb*G
+    kab = ka*kB
+    kba = kb*kA
+    kac = ka*kC
+    kca = kc*kA
+    kbc = kb*kC
+    kcb = kc*kB
     print(f'kab = {kab}')
     print(f'kba = {kba}')
     print(f'kac = {kac}')
@@ -152,16 +158,20 @@ if args.continuous:
 
 
 print ('Elliptic curve over finite fileds cryptography')
-a = -5 # args.a
-b = 8  # args.b
-p = 509 # args.p, no need to redefine
+a = args.a
+b = args.b
+p = args.prime
 curve = FiniteEllipticCurve(a, b, p)
 if args.debug:
     print(curve)
 if args.plot:
     curve.plot()
 
-G = FinitePoint(curve, 1, 507, 'G')
+solutions = solve_ecc(a, b, p)
+if solutions == []:
+    raise Error(f'Curve {curve} does not seem to have solutions.')
+x, y = solutions[0]
+G = FinitePoint(curve, x, y, 'G')
 if args.debug:
     print(f'G = {G}')
 if args.plot:
@@ -180,12 +190,12 @@ if args.plot:
     kC.plot('kC')
 
 print ('Pairwise session keys')
-kab = ka*kb*G
-kba = kb*ka*G
-kac = ka*kc*G
-kca = kc*ka*G
-kbc = kb*kc*G
-kcb = kc*kb*G
+kab = ka*kB
+kba = kb*kA
+kac = ka*kC
+kca = kc*kA
+kbc = kb*kC
+kcb = kc*kB
 print(f'kab = {kab}')
 print(f'kba = {kba}')
 print(f'kac = {kac}')
@@ -208,4 +218,4 @@ if args.plot:
     kabc1.plot('kabc1')
 
 if args.plot:
-    plt.savefig("graph_finite.png")
+    plt.savefig("graph_discrete.png")
